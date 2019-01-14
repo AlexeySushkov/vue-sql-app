e4961afe39676ae5d6d36945cb5f86c1e8b549ee
Alexey.Sushkov <Alexey.Sushkov@billing.ru>
2019-01-14 14:09:47 +0300

    Reg page added

---
+++ client/src/components/Register.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="id1">
+    <h1>Register</h1>
+
+    <input
+    type="email"
+    name="email"
+    v-model="email"
+    placeholder="email" />
+    <br>
+    <input
+    type="password"
+    name="password"
+    v-model="password"
+    placeholder="password" />
+    <br>
+    <button
+      @click="register">
+      Register
+    </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (value) {
+      console.log('email has changed: ', value)
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      // this.email = 'heloooooo2000'
+    }, 2000)
+  },
+  methods: {
+    async register () {
+      const resp = await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+      console.log(resp.data)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
